3cef64ce71cea3488176126d14c3adf72d0f8d34
Leticia Bernardeli <134503671+leticiabernardeli@users.noreply.github.com>
2023-08-22 09:08:36 -0300

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,26 @@
+function somavalores(){
+var s1 = document.getElementById("valor1").value;
+var s2 = document.getElementById("valor2").value;
+var s3 = parseInt(s1) + parseInt(s2);
+alert(s3);
+}
+
+function subtrairvalores(){
+    var s1 = document.getElementById("valor1").value;
+    var s2 = document.getElementById("valor2").value;
+    var s3 = parseInt(s1) - parseInt(s2);
+    alert(s3);}
+
+    function multplicarvalores(){
+        var s1 = document.getElementById("valor1").value;
+        var s2 = document.getElementById("valor2").value;
+        var s3 = parseInt(s1) * parseInt(s2);
+        alert(s3);}
+    
+
+        function dividirvalores(){
+            var s1 = document.getElementById("valor1").value;
+            var s2 = document.getElementById("valor2").value;
+            var s3 = parseInt(s1) / parseInt(s2);
+            alert(s3);}
+
